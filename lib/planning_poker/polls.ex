855a5927bb56f767327c9732afefc9403088ce0f
Leon Cruz <leon.cruz.teixeira@gmail.com>
2022-10-18 00:24:31 -0300

    feat: create context for polls

---
+++ lib/planning_poker/polls.ex
@@ -0,0 +1,24 @@
+defmodule PlanningPoker.Polls do
+  alias PlanningPoker.Polls.Room
+  alias PlanningPoker.Repo
+
+  import Ecto.Query
+
+  def list_rooms_by_user(user_id) do
+    query =
+      from r in Room,
+        where: r.user_id == ^user_id
+
+    Repo.all(query)
+  end
+
+  def room_changeset(room, params \\ %{}) do
+    Room.changeset(room, params)
+  end
+
+  def create_room(room, attrs) do
+    room
+    |> Room.changeset(attrs)
+    |> Repo.insert()
+  end
+end
