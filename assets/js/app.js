2ca086c68ee628438801ad472219b3daa5b2d61f
Leon Cruz <leon.cruz.teixeira@gmail.com>
2022-10-22 21:53:36 -0300

    feat: basic setup for channels

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,6 +3,8 @@
 import "../css/app.css"
 import "./shared/modal"
 
+import "./user_socket"
+
 // If you want to use Phoenix channels, run `mix help phx.gen.channel`
 // to get started and then uncomment the line below.
 // import "./user_socket.js"
